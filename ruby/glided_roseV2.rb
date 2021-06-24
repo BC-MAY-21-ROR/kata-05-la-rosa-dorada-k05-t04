1fd0b5bb2dfeae01b90fb2da9e85f7252d14be33
Marcos Gil <marcoswaldo52@gmail.com>
2021-06-24 11:48:58 -0500

    Defined new methods for more readable structure and use rubocop, sandimeter and rubycritic for improve the code
    Co-authored-by: Usiel-03A7 <shebastyan@gmail.com>

--- ruby/glided_roseV2.rb
+++ ruby/glided_roseV2.rb
@@ -1,68 +1,92 @@
+# frozen_string_literal: true
+
 class GildedRose
-
-    def initialize(items)
-      @items = items
-    end
+  def initialize(items)
+    @items = items
+  end
   # Ternary  suma<0 ? true : false
-   
-    def sumQuality (item)
-      item.quality += 1 
-    end
-    
-   def resQuality (item)
-    item.quality-=1
-   end
-   def quality50? (item)
-        item.quality<50
 
-    end 
-    def backstage (item)
-        if item.sell_in < 11
-            if item.quality < 50
-              sumQuality(item)
-            end
-          end
-        if item.sell_in < 6
-            if item.quality < 50
-              sumQuality(item)
-            end
-        end
-    end
-    
-    def sell_in?(item)
-        items.sell_in==0
-    end
-    def update_quality()
-      @items.each do |item|
-        case item.name
-        when 'Conjured Mana Cake'
-            resQuality(item)
-        when 'Aged Brie'
-            
-        when 'Backstage passes to a TAFKAL80ETC concert'
-        
-        when 'Sulfuras'
-            item.sell_in = nil
-            item.quality = item.quality
-        else
-            item.sell_in=-1
-        end    
-      end
-    end
-
+  def sumQuality(item)
+    item.quality += 1
   end
-  
-  class Item
-    attr_accessor :name, :sell_in, :quality
-  
-    def initialize(name, sell_in, quality)
-      @name = name
-      @sell_in = sell_in
-      @quality = quality
-    end
-  
-    def to_s()
-      "#{@name}, #{@sell_in}, #{@quality}"
+
+  def resQuality(item)
+    item.quality -= 1
+  end
+
+  def quality50?(item)
+    item.quality < 50
+  end
+
+  def backstage(item)
+    sumQuality(item) if item.sell_in < 11
+    sumQuality(item) if item.sell_in < 6 && (item.quality < 50)
+    item.quality -= item.quality if item.sell_in.negative?
+  end
+
+  def prodCalidS(item)
+    case item.name
+    when 'Aged Brie'
+      ageBrie item
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      backstage item
+    else
+      quality0 item
     end
   end
-  
+
+  def ageBrie(item)
+    sumQuality item
+    sumQuality item if item.sell_in.negative?
+  end
+
+  def prodCalidR(item)
+    if item.name == 'Conjured Mana Cake'
+      item.quality -= 2
+    else
+      resQuality item
+    end
+  end
+
+  def restSell_in(item)
+    item.sell_in -= 1
+  end
+
+  def notsulfuras?(item)
+    item.name != 'Sulfuras, Hand of Ragnaros'
+  end
+
+  def sell_in?(_item)
+    items.sell_in.zero?
+  end
+
+  def backstage0(item)
+    item.quality = 0
+  end
+
+  def quality0(item)
+    prodCalidR item if item.quality.positive?
+  end
+
+  def update_quality
+    @items.each do |item|
+      prodCalidS item if quality50? item
+      restSell_in item if notsulfuras? item
+      backstage0 item if item.sell_in.zero? && item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    end
+  end
+end
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
